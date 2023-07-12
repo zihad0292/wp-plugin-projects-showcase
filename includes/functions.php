d5919b68fdedf577c9e639a15bd9295648ef4a38
Zihad Ul Islam <zihad@scbd.com>
2023-07-13 01:21:45 +0600

    Implement register custom post types: 'wppool-zi-projects'

---
+++ includes/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+    /**
+	 * Registers the post type for WPPool Projects.
+	 */
+	function wppool_zi_projects_register_post_types() {
+		$labels = array(
+            'name'               => 'WPPool Projects',
+            'singular_name'      => 'WPPool Project',
+            'menu_name'          => 'WPPool Projects', 
+            'add_new'            => 'Add New',
+            'add_new_item'       => 'Add New WPPool Project',
+            'new_item'           => 'New WPPool Project',
+            'edit_item'          => 'Edit WPPool Project',
+            'view_item'          => 'View WPPool Project',
+            'all_items'          => 'All WPPool Projects',
+            'search_items'       => 'Search WPPool Projects',
+            'parent_item_colon'  => 'Parent WPPool Projects:',
+            'not_found'          => 'No WPPool Projects found.',
+            'not_found_in_trash' => 'No WPPool Projects found in Trash.'
+        );
+    
+        $args = array(
+            'labels'              => $labels,
+            'public'              => true,
+            'publicly_queryable'  => true,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'query_var'           => true,
+            'rewrite'             => array( 'slug' => 'wppool-zi-projects' ),
+            'capability_type'     => 'post',
+            'has_archive'         => true,
+            'hierarchical'        => false,
+            'menu_position'       => 5,
+            'menu_icon'           => 'dashicons-admin-post',
+            'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+        );
+    
+        register_post_type( 'wppool-zi-projects', $args );
+	}
--- wppool-zi-projects.php
+++ wppool-zi-projects.php
@@ -11,3 +11,31 @@
  * Text Domain: wppool-zi-projects
  * Domain Path: /languages
  */
+
+ 
+define( 'WPPOOL_ZI_PROJECTS_VERSION', '1.0.0' ); 
+
+define( 'WPPOOL_ZI_PROJECTS_TEXT_DOMAIN', 'wppool-zi-projects' );
+
+define( 'WPPOOL_ZI_PROJECTS_PLUGIN', __FILE__ );
+
+define( 'WPPOOL_ZI_PROJECTS_PLUGIN_BASENAME', plugin_basename( WPPOOL_ZI_PROJECTS_PLUGIN ) );
+
+define( 'WPPOOL_ZI_PROJECTS_PLUGIN_NAME', trim( dirname( WPPOOL_ZI_PROJECTS_PLUGIN_BASENAME ), '/' ) );
+
+define( 'WPPOOL_ZI_PROJECTS_PLUGIN_DIR', untrailingslashit( dirname( WPPOOL_ZI_PROJECTS_PLUGIN ) ) );
+ 
+require_once WPPOOL_ZI_PROJECTS_PLUGIN_DIR . '/includes/functions.php';
+
+
+/**
+ * Registers post types
+ */
+add_action( 'init', 'wppool_zi_projects_init', 10, 0 );
+
+function wppool_zi_projects_init() {
+	
+	wppool_zi_projects_register_post_types();
+
+	do_action( 'wppool_zi_projects_init' );
+}
