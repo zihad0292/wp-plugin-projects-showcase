ad87f12b1faed6ccb9fb2cec849aadd844237b09
Zihad Ul Islam <zihad@scbd.com>
2023-07-13 00:37:18 +0600

    Create plugin definition

---
+++ wppool-zi-projects.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WPPool Projects By Zihad
+ * Plugin URI: https://zihadsweb.com
+ * Description: This plugin extends the functionality of WordPress by adding a custom post types called 'Projects'. It provides a projects archive page and individual project pages displayed using a modal window. Additionally, it incorporates sorting and filtering capabilities for projects on the archive page.
+ * Version: 1.0.0
+ * Author: Zihad Ul Islam
+ * Author URI: https://zihadsweb.com
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: wppool-zi-projects
+ * Domain Path: /languages
+ */
