74c93c88465f64a13b9bc71ddae3ec885b601270
Zihad Ul Islam <zihad@scbd.com>
2023-07-17 11:33:24 +0600

    Cleanup code, add support for uploading multiple images, show already uploaded images below the upload button

--- assets/admin/js/main.js
+++ assets/admin/js/main.js
@@ -1,36 +1,51 @@
-var frame;
+var gframe;
 (function ($) {
   $(document).ready(function () {
-    var image_url = $("#wppool_zi_projects_image_url").val();
-    if (image_url) {
-      $("#image-container").html(`<img src='${image_url}' />`);
+    // show already uploaded preview images
+    var images_url = $("#wppool_zi_projects_images_url").val();
+    images_url = images_url ? images_url.split(";") : [];
+    for (i in images_url) {
+      var _image_url = images_url[i];
+      $("#images-container").append(
+        `<img style="margin-left: 10px" src='${_image_url}' />`
+      );
     }
 
-    $("#upload_image").on("click", function () {
-      if (frame) {
-        frame.open();
+    // handle upload preivew images
+    $("#upload_images").on("click", function () {
+      if (gframe) {
+        gframe.open();
         return false;
       }
 
-      frame = wp.media({
+      gframe = wp.media({
         title: "Select Image",
         button: {
           text: "Insert Image",
         },
-        multiple: false,
+        multiple: true,
       });
 
-      frame.on("select", function () {
-        var attachment = frame.state().get("selection").first().toJSON();
-        console.log(attachment);
-        $("#wppool_zi_projects_image_id").val(attachment.id);
-        $("#wppool_zi_projects_image_url").val(attachment.sizes.thumbnail.url);
-        $("#image-container").html(
-          `<img src='${attachment.sizes.thumbnail.url}' />`
-        );
+      gframe.on("select", function () {
+        var image_ids = [];
+        var image_urls = [];
+        var attachments = gframe.state().get("selection").toJSON();
+        //console.log(attachments);
+        $("#images-container").html("");
+        console.log(attachments);
+        for (i in attachments) {
+          var attachment = attachments[i];
+          image_ids.push(attachment.id);
+          image_urls.push(attachment.sizes.thumbnail.url);
+          $("#images-container").append(
+            `<img style="margin-right: 10px;" src='${attachment.sizes.thumbnail.url}' />`
+          );
+        }
+        $("#wppool_zi_projects_images_id").val(image_ids.join(";"));
+        $("#wppool_zi_projects_images_url").val(image_urls.join(";"));
       });
 
-      frame.open();
+      gframe.open();
       return false;
     });
   });
--- wppool-zi-projects.php
+++ wppool-zi-projects.php
@@ -19,10 +19,11 @@
 		add_action( 'init', array( $this, 'wppool_zi_projects_init' ) );
 		add_action( 'admin_menu', array( $this, 'wppool_zi_projects_add_metabox' ) );
 		add_action( 'save_post', array( $this, 'wppool_zi_projects_save_metabox' ) );
-		add_action( 'save_post', array( $this, 'wppool_zi_projects_save_image' ) );
+		add_action( 'save_post', array( $this, 'wppool_zi_projects_save_preview_images' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'wppool_zi_projects_admin_assets' ) );
 	}
 
+	// Enqueue css and js files
 	function wppool_zi_projects_admin_assets() {
 		wp_enqueue_style( 'wppool_zi_projects-admin-style', plugin_dir_url( __FILE__ ) . "assets/admin/css/style.css", null, time() );
 		wp_enqueue_style( 'jquery-ui-css', '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css', null, time() );
@@ -32,6 +33,7 @@
 	}
 
 
+	// Add security check for database operation
 	private function is_secured( $nonce_field, $action, $post_id ) {
 		$nonce = isset( $_POST[ $nonce_field ] ) ? $_POST[ $nonce_field ] : '';
 
@@ -59,6 +61,7 @@
 	}
 
  
+	// Register custom post type
 	function wppool_zi_projects_init() {
 		
 		wppool_zi_projects_register_post_types();
@@ -66,31 +69,57 @@
 		do_action( 'wppool_zi_projects_init' );
 	}
 
-	function wppool_zi_projects_save_image($post_id){ 
+	// Save metabox function
+	function wppool_zi_projects_save_metabox( $post_id ) {
 
-		$image_id    = isset( $_POST['wppool_zi_projects_image_id'] ) ? $_POST['wppool_zi_projects_image_id'] : '';
-		$image_url    = isset( $_POST['wppool_zi_projects_image_url'] ) ? $_POST['wppool_zi_projects_image_url'] : '';
+		if ( ! $this->is_secured( 'wppool_zi_projects_external_url_field', 'wppool_zi_projects_external_url', $post_id ) ) {
+			return $post_id;
+		}
 
-		update_post_meta($post_id,'wppool_zi_projects_image_id',$image_id);
-		update_post_meta($post_id,'wppool_zi_projects_image_url',$image_url);
+		$external_url    = isset( $_POST['wppool_zi_projects_external_url'] ) ? $_POST['wppool_zi_projects_external_url'] : ''; 
 
+		if ( $external_url == '' ) {
+			return $post_id;
+		}
+
+		$external_url = sanitize_text_field( $external_url ); 
+
+		update_post_meta( $post_id, 'wppool_zi_projects_external_url', $external_url ); 
 	}
 
-	function wppool_zi_projects_save_metabox( $post_id ) {
+	// Save preivew images
+	function wppool_zi_projects_save_preview_images($post_id){
+		if ( ! $this->is_secured( 'wppool_zi_projects_preview_images_nonce', 'wppool_zi_projects_preview_images', $post_id ) ) {
+			return $post_id;
+		}
+
+		$image_id    = isset( $_POST['wppool_zi_projects_images_id'] ) ? $_POST['wppool_zi_projects_images_id'] : '';
+		$image_url    = isset( $_POST['wppool_zi_projects_images_url'] ) ? $_POST['wppool_zi_projects_images_url'] : '';
+
+		update_post_meta($post_id,'wppool_zi_projects_images_id',$image_id);
+		update_post_meta($post_id,'wppool_zi_projects_images_url',$image_url);
 
 	}
 	
+	// Wrapper function for add_meta_box WP functions
 	function wppool_zi_projects_add_metabox() {
+		add_meta_box(
+			'wppool_zi_projects_post_external_url',
+			__( 'External URL', 'wppool-zi-projects' ),
+			array( $this, 'wppool_zi_projects_display_metabox' ),
+			'wppool_zi_projects',
+		); 
 
 		add_meta_box(
-			'wppool_zi_projects_image_info',
-			__( 'Image Info', 'our-metabox' ),
-			array( $this, 'wppool_zi_projects_image_info' ),
+			'wppool_zi_projects_preview_images',
+			__( 'Preview Images', 'wppool-zi-projects' ),
+			array( $this, 'wppool_zi_projects_preview_images' ),
 			'wppool_zi_projects',
 		);
 
 	}
 
+	// Single image upload button 
 	function wppool_zi_projects_image_info($post) {
 		$image_id = esc_attr(get_post_meta($post->ID,'wppool_zi_projects_image_id',true));
 		$image_url = esc_attr(get_post_meta($post->ID,'wppool_zi_projects_image_url',true));
@@ -117,6 +146,57 @@ EOD;
 		echo $metabox_html;
 
 	}
+
+	// Display upload image button for Preview Images
+	function wppool_zi_projects_preview_images($post) {
+		$image_id = esc_attr(get_post_meta($post->ID,'wppool_zi_projects_images_id',true));
+		$image_url = esc_attr(get_post_meta($post->ID,'wppool_zi_projects_images_url',true));
+		wp_nonce_field( 'wppool_zi_projects_preview_images', 'wppool_zi_projects_preview_images_nonce' );
+
+		$label = __('Preview Images','wppool-zi-projects');
+		$button_label = __('Upload Images','wppool-zi-projects');
+		$metabox_html = <<<EOD
+<div class="fields">
+	<div class="field_c">
+		<div class="label_c">
+			<label>{$label}</label>
+		</div>
+		<div class="input_c">
+			<button class="button" id="upload_images">{$button_label}</button>
+			<input type="hidden" name="wppool_zi_projects_images_id" id="wppool_zi_projects_images_id" value="{$image_id}"/>
+			<input type="hidden" name="wppool_zi_projects_images_url" id="wppool_zi_projects_images_url" value="{$image_url}"/>
+			<div id="images-container"></div>
+		</div>
+		<div class="float_c"></div>
+	</div>
+	
+</div>
+EOD;
+
+		echo $metabox_html;
+
+	}
+
+	// Display textfield metabox for External URL
+	function wppool_zi_projects_display_metabox( $post ) {
+		$external_url    = get_post_meta( $post->ID, 'wppool_zi_projects_external_url', true );
+
+		$label1 = __( 'External URL', 'wppool-zi-projects' );
+
+		wp_nonce_field( 'wppool_zi_projects_external_url', 'wppool_zi_projects_external_url_field' );
+
+
+		$metabox_html = <<<EOD
+<p>
+<label for="wppool_zi_projects_external_url">{$label1}: </label>
+<input type="text" name="wppool_zi_projects_external_url" id="wppool_zi_projects_external_url" value="{$external_url}"/>
+</p>
+EOD;
+
+		echo $metabox_html;
+	}
+
+
 }
 
 new WppoolZiProjects();
