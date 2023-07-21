a207f6113fe372d293ab3a6299429fd477c3b2f2
Zihad Ul Islam <zihad@scbd.com>
2023-07-21 12:02:43 +0600

    Add classes and css to achieve consistent styling across different themes

--- templates/archive-wppool_zi_projects.php
+++ templates/archive-wppool_zi_projects.php
@@ -45,7 +45,7 @@ $plugin_dir_url = plugin_dir_url( dirname( __FILE__ ) );
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h2 class="modal-title fs-6" id="singlePostModal">Modal title</h2>
+        <h2 class="modal-title fs-6 mt-0 mb-0" id="singlePostModal">Modal title</h2>
         <span class="btn-close" data-bs-dismiss="modal" aria-label="Close"></span>
       </div>
       <div class="modal-body">
@@ -67,8 +67,8 @@ $plugin_dir_url = plugin_dir_url( dirname( __FILE__ ) );
     <div class="archive-header jumbotron jumbotron-fluid bg-secondary text-light">
         <div class="overlay"></div>
         <div class="container">
-            <h1 class="display-4 fw-semibold">Portfolio projects</h1>
-            <p class="lead mt-2">A well-crafted portfolio is a window into one's professional journey, showcasing a diverse array of projects that highlight skills, expertise, and creativity. Each project encapsulates a unique story of dedication and problem-solving, reflecting the passion and commitment of the individual.</p>
+            <h1 class="display-4 fw-semibold text-light lh-base mt-0 mb-0">Portfolio projects</h1>
+            <p class="lead mt-2 text-light">A well-crafted portfolio is a window into one's professional journey, showcasing a diverse array of projects that highlight skills, expertise, and creativity. Each project encapsulates a unique story of dedication and problem-solving, reflecting the passion and commitment of the individual.</p>
         </div>
     </div>
     <section class="content py-5">
@@ -76,7 +76,7 @@ $plugin_dir_url = plugin_dir_url( dirname( __FILE__ ) );
             <div class="row flex-xl-nowrap">
                 <div class="col-sm-12 col-md-3 col-lg-2">
                     <div class="sidebar-row mb-3">
-                        <h4 class="fs-6 mb-3">Categories:</h4>
+                        <h4 class="fs-6 mt-0 mb-3 pb-0 text-dark">Categories:</h4>
                         <div id="filter-buttons">
                             <?php
                             // check if there are any projects available
@@ -89,16 +89,16 @@ $plugin_dir_url = plugin_dir_url( dirname( __FILE__ ) );
                                         echo '<a href="#" class="btn btn-secondary" data-filter=".' . $category->slug . '">' . $category->name . '</a>';
                                     }
                                 } else {
-                                    echo '<span class="fs-6 ps-3">No categories found!</span>';
+                                    echo '<p class="fs-6 ps-3 text-dark">No categories found!</p>';
                                 }  
                             }else{
-                                echo '<span class="fs-6 ps-3">No options available!</span>';
+                                echo '<p class="fs-6 ps-3 text-dark">No options available!</p>';
                             }
                             ?>    
                         </div><!-- #filter-buttons -->
                     </div><!-- sidebar-row -->
                     <div class="sidebar-row mb-5">
-                        <h4 class="fs-6 mb-3">Sort By:</h4>
+                        <h4 class="fs-6 mt-0 mb-3 pb-0 text-dark">Sort By:</h4>
                         <?php
                         // check if there are any projects available
                         if ( have_posts() ){ ?>
@@ -151,10 +151,10 @@ $plugin_dir_url = plugin_dir_url( dirname( __FILE__ ) );
                                 ?>
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-3 mb-md-4 wppool-loop-project element-item <?php echo $category_classes;?>" data-title="<?php the_title();?>" data-category="<?php echo ! empty( $category_names ) ? $category_names : "0";?>" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                                     <div class="card ajax-load-post" data-post-id="<?php the_ID();?>" data-title="<?php the_title();?>" data-bs-toggle="modal" data-bs-target="#single-post-modal">
-                                    <?php echo '<img class="card-img-top" src="' . esc_url( $thumbnail_url ) . '" alt="' . get_the_title() . '">'; ?> 
+                                    <?php echo '<img class="card-img-top d-block w-100" src="' . esc_url( $thumbnail_url ) . '" alt="' . get_the_title() . '">'; ?> 
                                         <div class="card-body">
-                                            <h5 class="card-title fs-5 fw-normal text-center"><?php the_title(); ?></h5>  
-                                            <p class="fs-6 fw-medium text-center wppool-loop-project-category">
+                                            <h5 class="card-title fs-5 fw-normal text-center pb-0 mt-0 mb-3"><?php the_title(); ?></h5>  
+                                            <p class="fs-6 fw-medium text-center wppool-loop-project-category text-dark">
                                             <?php 
                                             // Check if categories are available and loop through them
                                             if ( ! is_wp_error( $post_categories ) && ! empty( $post_categories ) ) {
@@ -173,7 +173,7 @@ $plugin_dir_url = plugin_dir_url( dirname( __FILE__ ) );
                             ?> 
                         </div><!-- row projects-grid -->                   
                         <?php else: ?>
-                            <p class="text-center fs-5">No projects found!</p>
+                            <p class="text-center fs-5 text-dark">No projects found!</p>
                         <?php endif; ?>
                     </div><!-- wppool-grid-container -->                   
                 </div><!-- col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content -->
--- templates/template-parts/content.php
+++ templates/template-parts/content.php
@@ -3,10 +3,10 @@
         <div class="container">
             <div class="featured-image">
                 <!--Post Thumbnail  -->
-                <img src='<?php echo esc_url($thumbnail_url); ?>' alt="">
+                <img class="d-block w-100" src='<?php echo esc_url($thumbnail_url); ?>' alt="">
             </div>
             <!-- Post Title -->
-            <h1 class="display-6 fw-semibold text-dark mb-3 mt-4"><?php echo esc_html($post_title); ?></h1> 
+            <h1 class="display-6 fw-semibold text-dark mb-3 mt-4 pb-0"><?php echo esc_html($post_title); ?></h1> 
             <!-- Post Category -->
             <?php if (!empty($post_categories)) : ?>
                 <div class="post-category">
@@ -32,18 +32,18 @@
         <!-- Post Description -->
         <div class="highlight p-5 text-bg-light border border-secondary-subtle rounded">
             <div class="post-description">
-                <h4 class="fs-4 fw-semibold text-dark mb-3">Description:</h4> 
+                <h4 class="fs-4 fw-semibold text-dark mt-0 mb-3 pb-0">Description:</h4> 
                 <?php echo wp_kses_post(wpautop($post_content)); ?>
             </div>
-            <hr class="my-5 w-25">
+            <hr class="my-5 w-25 d-block m-auto">
             <?php if (!empty($preview_images)) : ?>
                 <div class="preview-images">
-                    <h4 class="fs-4 fw-semibold text-dark text-center">Preview Images:</h4>
-                    <p class="text-center">Click on any image to open in lightbox</p>
+                    <h4 class="fs-4 fw-semibold text-dark text-center mt-0 mb-0 pb-0">Preview Images:</h4>
+                    <p class="text-center text-dark">Click on any image to open in lightbox</p>
                     <div class="row mt-5">
                         <?php
                         foreach ( $preview_images as $index => $url ) { 
-                            echo '<div class="col-sm-6 col-md-4 col-lg-3 mb-3 mb-md-4"><div class="image-wrapper"><a data-lightbox="image" href="' . esc_url( $preview_images_full_size[$index] ) . '"><img class="card-img-top" src="' . esc_url( $url ) . '" alt=""></a></div></div>';
+                            echo '<div class="col-sm-6 col-md-4 col-lg-3 mb-3 mb-md-4"><div class="image-wrapper"><a data-lightbox="image" href="' . esc_url( $preview_images_full_size[$index] ) . '"><img class="card-img-top d-block w-100" src="' . esc_url( $url ) . '" alt=""></a></div></div>';
                         }
                         ?>
                     </div> 
